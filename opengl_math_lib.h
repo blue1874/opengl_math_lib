e16b4d4cd7498c28dad7ad0a54f9b22e1b954634
gbldyx <gbldyx@yeah.net>
2019-10-18 13:05:46 +0800

    Change README. Rename the header file.

--- opengl_math_lib.h
+++ opengl_math_lib.h
@@ -4,40 +4,86 @@
 /************************************
  *            type                  *
  ***********************************/
-typedef float* vec;
-typedef float* mat;
-typedef float* tr;
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+}Vec3;
+
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+	float w;
+}Vec4;
+
+typedef struct
+{
+	vec4 row1;
+	vec4 row2;
+	vec4 row3;
+	vec4 row4;
+	vec4 col1;
+	vec4 col2;
+	vec4 col3;
+	vec4 col4;
+}Mat4;
+
+typedef Mat4 Tr;
+typedef Mat4 Cam;
+
+/************************************
+ *            utils                 *
+ ***********************************/
+
+float to_radian(float);
 
 /************************************
  *           vector                 *
  ***********************************/
 
-vec vec_new();
-void vec_copy(vec, vec);
-void vec_add(vec, vec);
-void vec_scale(vec, float);
-void vec_dot(vec, vec);
-void vec_cross(vec, vec);
-void vec_normalize(vec);
-void vec_delete(vec);
+Vec3 vec3_new();
+float vec3_mod(Vec3);
+void vec3_copy(Vec3, Vec3);
+void vec3_add(Vec3, Vec3);
+void vec3_scale(Vec3, float);
+void vec3_dot(Vec3, Vec3);
+void vec3_cross(Vec3, Vec3);
+void vec3_normalize(Vec3);
+void vec3_delete(Vec3);
 
 /************************************
  *           matrix                 *
  ***********************************/
 
-mat mat_new();
-void mat_copy(mat, mat);
-void mat_add(mat, mat);
-void mat_mult_mat(mat, mat);
-void mat_print(mat);
-void mat_delete(mat);
+Mat mat_new();
+void mat_copy(Mat, Mat);
+void mat_add(Mat, Mat);
+void mat_mult_mat(Mat, Mat);
+void mat_print(Mat);
+void mat_delete(Mat);
 
 /************************************
  *           transform              *
  ***********************************/
 
-tr tr_new();
-void tr_trans(tr, float, float, float);
-void tr_rotate(tr, float, float, float);
-void tr_scale(tr, float, float, float);
-void tr_delete(tr);
+Tr tr_new();
+void tr_trans(Tr, float, float, float);
+void tr_rotate(Tr, float, float, float);
+void tr_scale(Tr, float, float, float);
+void tr_delete(Tr);
+
+/************************************
+ *           projection             *
+ ***********************************/
+
+Mat ortho_proj(float, float, float, float, float, float);
+Mat persp_proj(float, float, float, float);
+
+/************************************
+ *            camera                *
+ ***********************************/
+
+Cam look_at(Vec3, Vec3, Vec3);
