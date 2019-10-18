c5d1ca97207847c5f6374b3cad1ea7dd5e4db528
blue <bluelizhijian@gmail.com>
2019-10-18 21:15:32 +0800

    definition

--- opengl_math_lib.cpp
+++ opengl_math_lib.cpp
@@ -4,62 +4,103 @@
  *           vector                 *
  ***********************************/
 
-Vec vec_new(float x = 0, float y = 0, float z = 0, float w = 0)
+Mat4f::Mat4f()
 {
-    Vec vec;
-    vec.x = x;
-    vec.y = y;
-    vec.z = z;
-    vec.w = w;
-    return vec;
+    for(int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            m[i][j] = 0.0;
+        }
+        
+    }
 }
 
-float vec_mod(Vec p)
+Mat4f::Mat4f(float x)
 {
-    return sqrt(p.x * p.x + p.y * p.y + p.z * p.z + p.w * p.w);
+    for(int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            m[i][j] = i == j ? x : 0;
+        }
+    }    
 }
 
-void vec_copy(Vec &p1, Vec p2)
+Mat4f::Mat4f(float x0, float y0, float z0, float w0, 
+			 float x1, float y1, float z1, float w1, 
+			 float x2, float y2, float z2, float w2, 
+			 float x3, float y3, float z3, float w3)
+             {
+                 m[0][0] = x0; m[0][0] = y0; m[0][0] = z0; m[0][0] = w0; 
+                 m[0][0] = x1; m[0][0] = y1; m[0][0] = z1; m[0][0] = w1; 
+                 m[0][0] = x2; m[0][0] = y2; m[0][0] = z2; m[0][0] = w2; 
+                 m[0][0] = x3; m[0][0] = y3; m[0][0] = z3; m[0][0] = w3;
+             }
+             
+Mat4f::Mat4f(const Mat4f &x)
 {
-    p1.x = p2.x;
-    p1.y = p2.y;
-    p1.z = p2.z;
-    p1.w = p2.w;
+    for(int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            m[i][j] = x.m[i][j];
+        }
+    }        
 }
 
-void vec_add(Vec &p1, Vec p2)
+Mat4f Mat4f::operator+(const Mat4f &x)
 {
-    p1.x += p2.x;
-    p1.y += p2.y;
-    p1.z += p2.z;
-    p1.w += p2.w;
+    Mat4f y(0);
+    for(int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            y.m[i][j] = m[i][j] + x.m[i][j];
+        }
+    }
+    return y;     
 }
 
-void vec_scale(Vec &p1, float scale)
+Mat4f Mat4f::operator*(const Mat4f &x)
 {
-    p1.x *= scale;
-    p1.y *= scale;
-    p1.z *= scale;
-    p1.w *= scale;
+    Mat4f y(0);
+    for(int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                y.m[i][j] += m[i][k] * x.m[k][j];
+            }            
+        }
+    }
+    return y;    
+}
+Vector4f Mat4f::operator*(const Vector4f &x)
+{
+    Vector4f y;
+    for(int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            y.m[i] += m[i][j] * x.m[j];           
+        }
+    }
+    return y;     
 }
 
-float vec_dot(Vec p1, Vec p2)
+void Mat4f::print(const Mat4f &x)
 {
-    return p1.x * p2.x + p1.y * p2.y + p1.z * p2.z + p1.w * p2.w;
+    for(int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%f\t", x.m[i][j]);
+        }
+        printf("\n");
+    }    
 }
 
-void vec_cross(Vec &p1, Vec p2)
-{
-    
-}
-
-void vec_normalize(Vec &p)
-{
-    float mod = vec_mod(p);
-    p.x /= mod;
-    p.y /= mod;
-    p.z /= mod;
-    p.w /= mod;
-}
-
-void vec_delete(Vec);
+Mat4f::~Mat4f()
+{}
--- opengl_math_lib.h
+++ opengl_math_lib.h
@@ -7,22 +7,15 @@
 /************************************
  *            type                  *
  ***********************************/
-/*typedef struct
-{
-	float x;
-	float y;
-	float z;
-}Vec3;*/
 
+
+/************************************
+ *           vector                 *
+ ***********************************/
 class Vector4f
 {
-private:
-	float x;
-	float y;
-	float z;
-	float w;
-
 public:
+	float x, y, z, w;
 	Vector4f(float, float, float, float);
 	Vector4f(const Vector4f &);
 	Vector4f operator+(const Vector4f &);
@@ -34,101 +27,48 @@ public:
 	~Vector4f();
 };
 
+
+/************************************
+ *           matrix                 *
+ ***********************************/
+
 class Mat4f
 {
-private:
-	float m[4][4];
 public:
-	Mat4f(/* args */);
-	Mat4f mat_add(Mat, Mat);
-	void mat_mult_mat(Mat, Mat);
-	void mat_print(Mat);
+	float m[4][4];
+	Mat4f();
+	Mat4f(float);
+	Mat4f(float, float, float, float, 
+			float, float, float, float, 
+			float, float, float, float, 
+			float, float, float, float);
+	Mat4f(const Mat4f &);
+	Mat4f operator+(const Mat4f &);
+	Mat4f operator*(const Mat4f &);
+	Vector4f operator*(const Vector4f &);
+	void print(const Mat4f &);
 	~Mat4f();
 };
 
-Mat4f::Mat4f(/* args */)
-{
-}
-
-Mat4f::~Mat4f()
-{
-}
-
-typedef struct
-{
-	float x;
-	float y;
-	float z;
-	float w;
-} Vec;
-
-typedef struct
-{
-	Vec row1;
-	Vec row2;
-	Vec row3;
-	Vec row4;
-	Vec col1;
-	Vec col2;
-	Vec col3;
-	Vec col4;
-} Mat;
-
-typedef Mat Tr;
-typedef Mat Cam;
 
 /************************************
  *            utils                 *
  ***********************************/
 
 float to_radian(float);
-
-/************************************
- *           vector                 *
- ***********************************/
-
-Vec vec_new(float, float, float, float);
-float vec_mod(Vec);
-void vec_copy(Vec, Vec);
-void vec_add(Vec, Vec);
-void vec_scale(Vec, float);
-float vec_dot(Vec, Vec);
-void vec_cross(Vec, Vec);
-void vec_normalize(Vec);
-void vec_delete(Vec);
-
-/************************************
- *           matrix                 *
- ***********************************/
-
-Mat mat_new();
-void mat_copy(Mat, Mat);
-void mat_add(Mat, Mat);
-void mat_mult_mat(Mat, Mat);
-void mat_print(Mat);
-void mat_delete(Mat);
-
 /************************************
  *           transform              *
  ***********************************/
 
-Tr tr_new();
-void tr_trans(Tr, float, float, float);
-void tr_rotate(Tr, float, float, float);
-void tr_scale(Tr, float, float, float);
-void tr_delete(Tr);
 
 /************************************
  *           projection             *
  ***********************************/
 
-Mat ortho_proj(float, float, float, float, float, float);
-Mat persp_proj(float, float, float, float);
 
 /************************************
  *            camera                *
  ***********************************/
 
-Cam look_at(Vec, Vec, Vec);
 
 #endif
